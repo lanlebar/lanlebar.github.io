e1bd9fa3f7936395daa7fcb6e639f57ac6ab6d0c
Lan <lanlebar5@gmail.com>
2024-07-10 17:05:19 +0200

    starting to seperate

---
+++ header.php
@@ -0,0 +1,50 @@
+<!-- header.php -->
+<header class="container-fluid">
+    <nav class="navbar navbar-expand-lg">
+        <a class="navbar-brand" href="index.php"><img src="images/logo.png" alt="Mlin transport logo"></a>                  
+        <button class="toggler collapsed d-flex d-lg-none flex-column justify-content-around" type="button" data-bs-toggle="collapse" data-bs-target="#navbar-dropdown" aria-controls="navbar-dropdown" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="toggler-icon top-bar"></span>
+            <span class="toggler-icon middle-bar"></span>
+            <span class="toggler-icon bottom-bar"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbar-dropdown">
+            <ul class="navbar-nav ms-auto d-flex align-items-center">
+                <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle text-center" href="#" id="o-nas-dropdown-link" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                        O nas
+                    </a>
+                    <div class="dropdown-menu text-center" aria-labelledby="o-nas-dropdown-link">
+                        <a class="dropdown-item" href="o-nas/o-podjetju.html">O podjetju</a>
+                        <a class="dropdown-item" href="o-nas/poslovni-partnerji.html">Poslovni partnerji</a>
+                        <a class="dropdown-item" href="o-nas/socialno.html">Socialno</a>
+                        <a class="dropdown-item" href="o-nas/zaposlitev.html">Zaposlitev</a>
+                    </div>
+                </li>
+                <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle text-center" href="#" id="storitve-dropdown-link" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                        Storitve
+                    </a>
+                    <div class="dropdown-menu text-center" aria-labelledby="storitve-dropdown-link">
+                        <a class="dropdown-item" href="storitve/vozni-park.html">Vozni park</a>
+                        <a class="dropdown-item" href="storitve/vrste-prevozov.html">Vrste prevozov</a>
+                        <a class="dropdown-item" href="storitve/logistika.html">Logistika</a>
+                    </div>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="galerija.html">Galerija</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="kontakt.html">Kontakt</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="poslji-povprasevanje.html">Pošlji povpraševanje</a>
+                </li>
+                <li class="nav-item">
+                    <a href="en/index.php">
+                        <img src="images/slovenia.png" alt="Slovensko" style="width: 2em; margin-left: 0.5em;">
+                    </a>
+                </li>                        
+            </ul>
+        </div>
+    </nav>
+</header>
